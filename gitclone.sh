71612a75ccaff0d59e79c66d91b5ace367a6cb42
David Manahan <dmanahan@usc.edu>
2018-02-02 13:31:05 -0800

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -6,11 +6,15 @@ make
 wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.7.zip
 unzip -vf fastqc_v0.11.7.zip
 
-git clone https://github.com/dmanahan/TrimGalore.git
-
-git clone https://github.com/dmanahan/cutadapt.git
+curl -fsSL https://github.com/FelixKrueger/TrimGalore/archive/0.4.3.tar.gz -o trim_galore.tar.gz
+tar xvzf trim_galore.tar.gz
 
 wget https://github.com/alexdobin/STAR/archive/2.5.3a.tar.gz
 tar -xzf 2.5.3a.tar.gz
 
-git clone https://github.com/dmanahan/htslib.git
+git clone https://github.com/samtools/samtools.git
+cd samtools
+make
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+~/miniconda3/bin/conda install -c bioconda cutadapt
