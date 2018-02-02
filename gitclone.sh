b59db2066bb06f63d1a4eae26fcb921b35c484d0
David Manahan <dmanahan@usc.edu>
2018-02-02 10:28:51 -0800

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/dmanahan/sra-tools.git
-git clone https://github.com/dmanahan/fastqc.git
+wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.7.zip
 git clone https://github.com/dmanahan/TrimGalore.git
 git clone https://github.com/dmanahan/cutadapt.git
 git clone https://github.com/dmanahan/STAR.git
