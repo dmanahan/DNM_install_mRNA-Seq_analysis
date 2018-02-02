c8ce52ddad23f9acf2cd22f5670ead10d71dfd49
David Manahan <dmanahan@usc.edu>
2018-02-02 10:16:25 -0800

    Create gitclone.sh

---
+++ gitclone.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/dmanahan/sra-tools.git
+git clone https://github.com/dmanahan/fastqc.git
+git clone https://github.com/dmanahan/TrimGalore.git
+git clone https://github.com/dmanahan/cutadapt.git
+git clone https://github.com/dmanahan/STAR.git
+git clone https://github.com/dmanahan/htslib.git
